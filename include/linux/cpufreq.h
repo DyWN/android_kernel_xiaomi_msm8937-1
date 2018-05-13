7b96c83776e0cc58659d0ec8c218395b44fab4f7
klozz <xxx.reptar.rawrr.xxx@gmail.com>
2018-05-13 11:38:28 +0200

    add alessa_gov alessa_term and alessa_plug by klozz (TeamMEX)

--- include/linux/cpufreq.h
+++ include/linux/cpufreq.h
@@ -534,6 +534,9 @@ extern struct cpufreq_governor cpufreq_gov_cultivation;
 #elif defined(CONFIG_CPU_FREQ_DEFAULT_GOV_SCHED)
 extern struct cpufreq_governor cpufreq_gov_sched;
 #define CPUFREQ_DEFAULT_GOVERNOR	(&cpufreq_gov_sched)
+#elif defined(CONFIG_CPU_FREQ_DEFAULT_GOV_ALESSA)
+extern struct cpufreq_governor cpufreq_gov_alessa;
+#define CPUFREQ_DEFAULT_GOVERNOR (&cpufreq_gov_alessa)
 #endif
 
 /*********************************************************************
